f4157382f63c01904ace7617d1456eed0db9c79e
613S035\sse <690829795@qq.com>
2016-09-20 17:56:20 +0800

    finish homework.

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 function print(n) {
     for (var i = 1; i <= n; i++) {
+        var str = "";
         for (var x = 0; x < n - i; x++) {
-            console.log("");
+            str += " ";
         }
         for (var y = 0; y < i; y++) {
-            console.log("*");
+            str += "*";
         }
+        console.log(str);
     }
 }
 var n = process.argv[2];
--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 function print(n:number):void{
+    
     for(var i=1;i<=n;i++){
+        var str:string="";
         for(var x=0;x<n-i;x++){
-             console.log("");
+              str +=" ";
         }
+       
         for(var y=0;y<i;y++){
-             console.log("*");
+            str +="*";
 
         }
-        console.log("\n");
+        console.log(str);
+        
     }
 }
 var n:string = process.argv[2];
